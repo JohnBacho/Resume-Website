5b8dccb0e1d004f8a344193a84b42ddfe2f6882f
JohnBacho <jbacho22@bw.edu>
2026-06-06 13:17:24 -0400

    fixed issue with key in projectboxes

--- src/Home/ProjectBoxes.jsx
+++ src/Home/ProjectBoxes.jsx
@@ -4,17 +4,15 @@ export default function Box({ props }) {
   return (
     <div className="Box--Container ProjectBoxs">
       {props.map((item, index) => (
-        <>
-          <div
-            key={index}
-            className="Box--Content"
-            onClick={() => (window.location.href = item.link)}
-          >
-            <h3>{item.title}</h3>
-            <h1>{item.project}</h1>
-            <img src={item.image} alt={item.title}></img>
-          </div>
-        </>
+        <div
+          key={index}
+          className="Box--Content"
+          onClick={() => (window.location.href = item.link)}
+        >
+          <h3>{item.title}</h3>
+          <h1>{item.project}</h1>
+          <img src={item.image} alt={item.title}></img>
+        </div>
       ))}
     </div>
   );
